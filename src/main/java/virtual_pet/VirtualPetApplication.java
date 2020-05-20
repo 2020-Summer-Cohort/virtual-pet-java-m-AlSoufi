e8f3a6907d463738e1d40401ad62fda51b7b01ed
m-alsoufi <mo7ammad.alsoufi@gmail.com>
2020-05-20 00:37:40 -0400

    corrected code being the wrong class error

--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -3,37 +3,3 @@ package virtual_pet;
 import java.util.Scanner;
 
 public class VirtualPetApplication {
-    private int hunger;
-    private int thirst;
-    private int waste;
-    private int boredom;
-    private int sickness;
-
-    public VirtualPetApplication(int hunger, int thirst, int waste, int boredom, int sickness) {
-        this.hunger = hunger;
-        this.thirst = thirst;
-        this.waste = waste;
-        this.boredom = boredom;
-        this.sickness = sickness;
-    }
-
-    public int getHunger() {
-        return hunger;
-    }
-
-    public int getThirst() {
-        return thirst;
-    }
-
-    public int getWaste() {
-        return waste;
-    }
-
-    public int getBoredom() {
-        return boredom;
-    }
-
-    public int getSickness() {
-        return sickness;
-    }
-}
--- src/main/java/virtual_pet/Virtualpet.java
+++ src/main/java/virtual_pet/Virtualpet.java
@@ -2,6 +2,47 @@ package virtual_pet;
 
 public class Virtualpet {
 
+    private String name;
+    private int hunger;
+    private int thirst;
+    private int waste;
+    private int boredom;
+    private int sickness;
+
+    public Virtualpet(String name, int hunger, int thirst, int waste, int boredom, int sickness) {
+        this.name = name;
+        this.hunger = hunger;
+        this.thirst = thirst;
+        this.waste = waste;
+        this.boredom = boredom;
+        this.sickness = sickness;
+    }
+
+    public void feeding(){
+
+    }
+
+    public int getHunger() {
+        return hunger;
+    }
+
+    public int getThirst() {
+        return thirst;
+    }
+
+    public int getWaste() {
+        return waste;
+    }
+
+    public int getBoredom() {
+        return boredom;
+    }
+
+    public int getSickness() {
+        return sickness;
+    }
 }
+
+
 
 
